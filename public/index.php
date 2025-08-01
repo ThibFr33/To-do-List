f8bcc718651ea328dae5f5a646bd51204d2a765b
ThibFr33 <thibs.cha@gmail.com>
2025-08-01 17:48:35 +0200

    first commit, mise en place de l'environnement de travail php, html, css, js

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/script.js"></script>
+    <title>Our To-Do List App</title>
+</head>
+<body>
+
+<h1>Ceci est la 1ère page de notre site !!! Bon courage la team @yusuf et @mickael</h1>
+<h2>page trouvable sous : http://localhost/To-do-List/public/index.php</h2>
+    
+</body>
+</html>
